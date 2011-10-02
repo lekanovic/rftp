14a6e5e1f476b68ddb5951c3e0e1338d1cbde9c5
Radovan Lekanovic <lekanovic@gmail.com>
2011-10-02 03:06:15 +0200

    Added features: LIST,PORT,PWD
    Now handling LIST,PORT and PWD commands to
    some degree.

--- src/parser.c
+++ src/parser.c
@@ -2,12 +2,19 @@
 #include <string.h>
 #include <errno.h>
 #include <unistd.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <netinet/in.h>
+#include <dirent.h>
 /* CR \r
  * LF \n
  */
 #define WELCOME_MSG	"220 Raddes ftp server beta v1.0\r\n"
 #define LOGIN_MSG	"331 Anonymous access allowed\r\n"
-#define SYSTEM_TYPE	"Ubunut 11.04 Intel Core i7\r\n"
+#define SYSTEM_TYPE	"211 Ubuntu 11.04 Intel Core i7\r\n"
+#define PWD_OK	"257 /radde/padde\r\n"
+#define OPEN_ASCII_MODE	"150 Opening ASCII mode data connection for file list\r\n"
+#define GOODBYE	"221 Goodbye\r\n"
 
 static ssize_t ftp_recv(int sockfd, void *buf, size_t len, int flags)
 {
@@ -16,7 +23,7 @@ static ssize_t ftp_recv(int sockfd, void *buf, size_t len, int flags)
 	if ((bytes = recv(sockfd,buf,len,0)) < 0)
 		printf("%s line %d\n",strerror(errno),__LINE__);
 
-	printf("[DATA:recv] %s\n",(char*)buf);
+	printf("[DATA:recv:fd=%d] %s\n",sockfd,(char*)buf);
 
 	return bytes;
 }
@@ -24,7 +31,7 @@ static ssize_t ftp_send(int sockfd, const void *buf, size_t len, int flags)
 {
 	int bytes;
 
-	printf("[DATA:send] %s\n",(char*)buf);
+	printf("[DATA:send:fd=%d] %s\n",sockfd,(char*)buf);
 
 	if ((bytes = send(sockfd,buf,len,0)) < 0)
 		printf("%s line %d\n",strerror(errno),__LINE__);
@@ -36,7 +43,7 @@ static ssize_t ftp_send(int sockfd, const void *buf, size_t len, int flags)
 int handle_msg(int client_sfd)
 {
 	char buf[1024];
-	int bytes;
+	int bytes,response;
 
 	if (send(client_sfd,WELCOME_MSG,strlen(WELCOME_MSG),0) < 0)
 		printf("%s line %d\n",strerror(errno),__LINE__);
@@ -46,9 +53,10 @@ int handle_msg(int client_sfd)
 			printf("Client closed connection\n");
 			return 0;
 		}
-		
-		if (parse_msg(buf) < 0) {
+		response = parse_msg(client_sfd,buf);
+		if (response < 0) {
 			printf("Client exited, bye\n\r");
+			ftp_send(client_sfd,GOODBYE,strlen(GOODBYE),0);
 			break;
 		}
 
@@ -60,8 +68,9 @@ int handle_msg(int client_sfd)
 	printf("echo msg exit\n");
 	return 0;
 }
+int port_fd;
 
-int parse_msg(char* msg)
+int parse_msg(int client_sfd,char* msg)
 {
 	if (strstr(msg,"?") != NULL) {
 		printf("%s %d %s\n",__func__,__LINE__,msg);
@@ -96,6 +105,8 @@ int parse_msg(char* msg)
 		printf("%s %d %s\n",__func__,__LINE__,msg);
 	} else if ( strstr(msg,"LIST") != NULL) {
 		printf("%s %d %s\n",__func__,__LINE__,msg);
+		ftp_send(client_sfd,OPEN_ASCII_MODE,strlen(OPEN_ASCII_MODE),0);
+		return handle_list(msg);
 	} else if ( strstr(msg,"LPRT") != NULL) {
 		printf("%s %d %s\n",__func__,__LINE__,msg);
 	} else if ( strstr(msg,"LPSV") != NULL) {
@@ -128,12 +139,12 @@ int parse_msg(char* msg)
 		printf("%s %d %s\n",__func__,__LINE__,msg);
 	} else if ( strstr(msg,"PORT") != NULL) {
 		printf("%s %d %s\n",__func__,__LINE__,msg);
+		port_fd = handle_port(msg);
 	} else if ( strstr(msg,"PROT") != NULL) {
 		printf("%s %d %s\n",__func__,__LINE__,msg);
 	} else if ( strstr(msg,"PWD") != NULL) {
 		printf("%s %d %s\n",__func__,__LINE__,msg);
-		memset(msg,0,1024);
-		getcwd(msg,1024);
+		handle_pwd(msg);
 	} else if ( strstr(msg,"QUIT") != NULL) {
 		printf("%s %d %s\n",__func__,__LINE__,msg);
 		return -1;
@@ -179,6 +190,76 @@ int parse_msg(char* msg)
 	return 0;
 }
 
+int handle_list(char* msg)
+{
+	DIR *dir;
+	struct dirent *dent;
+	int bytes=0;
+
+	memset(msg,0,1024);
+	dir = opendir(".");
+
+	while ((dent = readdir(dir)) != NULL) {
+		bytes += strlen(dent->d_name);
+		if ( bytes >= 1024 ) break;
+		strcat(msg,dent->d_name);
+		strcat(msg," ");
+	}
+	strcat(msg,"\r\n");
+	ftp_send(port_fd,msg,strlen(msg),0);
+
+	memset(msg,0,1024);
+	strcpy(msg,"226 Transfer complete\r\n");
+	close(port_fd);
+
+	return 2;
+}
+int handle_pwd(char* msg)
+{
+	char tmp[512];
+
+	memset(tmp,0,512);
+	getcwd(tmp,512);
+	memset(msg,0,1024);
+	strcpy(msg,"250 ");
+	strcat(msg,tmp);
+	strcat(msg,"\r\n");
+
+	return 0;
+}
+
+int handle_port(char* msg)
+{
+	int fd;
+	char* pch;
+	char tmp[10];
+	int ip[6],i=0;
+	struct sockaddr_in serv_addr;
+
+	pch = strtok(msg," ,");
+	pch = strtok(NULL," ,");
+
+	while (pch != NULL) {
+		ip[i++] = atoi(pch);
+		pch = strtok(NULL," ,");
+	}
+
+	memset(tmp,0,10);
+	sprintf(tmp,"%x%x",ip[4],ip[5]);
+
+	serv_addr.sin_family = AF_INET;
+	serv_addr.sin_port = htons(strtol(tmp,NULL,16));
+	serv_addr.sin_addr.s_addr = inet_addr("127.0.0.1");
+
+	fd = socket(AF_INET,SOCK_STREAM,0);
+	if (connect(fd,(struct sockaddr *)&serv_addr,sizeof(serv_addr)) < 0)
+		printf("%s %d %s port %d\n",__func__,__LINE__,
+				strerror(errno),serv_addr.sin_port);
+
+	memset(msg,0,1024);
+	strcpy(msg, "200 PORT command successful\r\n");
+	return fd;
+}
 
 int echo_msg(int client_sfd)
 {
@@ -211,42 +292,6 @@ int echo_msg(int client_sfd)
 	printf("echo msg exit\n");
 	return 0;
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
