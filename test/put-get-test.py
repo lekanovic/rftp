f0b5797d0103e932cfa6e7b19e57a360d90cdc1a
Radovan Lekanovic <lekanovic@gmail.com>
2011-10-09 20:43:57 +0200

    Added test-case for MKD and RMD commands.
    Test if MKD and RMD command works as they
    should.

--- test/put-get-test.py
+++ test/put-get-test.py
@@ -44,12 +44,25 @@ def retr_cmd_test(filename):
 	# fetch a text file
 	outfile = sys.stdout
 	# use a lambda to add newlines to the lines read from the server
-	ftp.retrlines("RETR " + filename, lambda s, w=outfile.write: w(s+"\n"))
-
+	try:
+		ftp.retrlines("RETR " + filename, lambda s, w=outfile.write: w(s+"\n"))
+	except :
+		print_test_failed()
 def stor_cmd_test(filename):
 	#print ftp.retrlines('STOR testfile.txt')
 	ftp.storlines("STOR " + filename, open(filename))
 
+def mkd_cmd_test():
+	if ftp.sendcmd('MKD radde') != "257 radde directory created":
+		print_test_failed()
+	else:
+		print_test_passed()
+
+def rmd_cmd_test():
+	if ftp.sendcmd('RMD radde') != "250 Requested file action okay, completed":
+		print_test_failed()
+	else:
+		print_test_passed()
 def main(argv):
 	connect_to_ftp()
 
@@ -58,7 +71,8 @@ def main(argv):
 	cwd_cmd_test()
 	#stor_cmd_test("testfile.txt")
 	retr_cmd_test("testfile.txt")
-
+	mkd_cmd_test()
+	rmd_cmd_test()
 	close_connection()
 
 if __name__ == "__main__":
