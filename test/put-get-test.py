c5935a765aaf839f03d1466e703ee01f0207b226
Radovan Lekanovic <lekanovic@gmail.com>
2011-10-09 19:03:48 +0200

    Updated test-case.
    Testing PWD,CWD and RETR commands.

--- test/put-get-test.py
+++ test/put-get-test.py
@@ -2,11 +2,69 @@
 import ftplib
 import sys
 
-def main(argv):
-	ftp = ftplib.FTP()
+#http://docs.python.org/library/ftplib.html
+
+ftp = ftplib.FTP()
+
+def print_test_failed():
+	print "\033[01;31mTEST FAILED\033[0m"
+
+def print_test_passed():
+	print "\033[01;34mTEST PASSED\033[0m"
+
+def connect_to_ftp():
 	ftp.connect('localhost',7000)     #specify port number when connection
 	ftp.login('radovan','XXX')
+	ftp.set_pasv(False)
+
+def close_connection():
+	ftp.close()
+
+def list_cmd_test():
 	ftp.retrlines('LIST')
+
+def pwd_cmd_test():
+	if ftp.sendcmd('PWD') != "226 Transfer complete":
+		print_test_failed()
+	else:
+		print_test_passed()
+
+def cwd_cmd_test():
+	if ftp.sendcmd('CWD test') != "200 Working directory changed":
+		print_test_failed()
+	else:
+		print_test_passed()
+
+	if ftp.sendcmd('CWD ..') != "200 Working directory changed":
+		print_test_failed()
+	else:
+		print_test_passed()
+
+def retr_cmd_test(filename):
+	# fetch a text file
+	outfile = sys.stdout
+	# use a lambda to add newlines to the lines read from the server
+	ftp.retrlines("RETR " + filename, lambda s, w=outfile.write: w(s+"\n"))
+
+def stor_cmd_test(filename):
+	#print ftp.retrlines('STOR testfile.txt')
+	ftp.storlines("STOR " + filename, open(filename))
+
+def main(argv):
+	connect_to_ftp()
+
+	list_cmd_test()
+	pwd_cmd_test()
+	cwd_cmd_test()
+	#stor_cmd_test("testfile.txt")
+	retr_cmd_test("testfile.txt")
+
+	close_connection()
 
 if __name__ == "__main__":
 	main(sys.argv[1:])
+
+
+
+
+
