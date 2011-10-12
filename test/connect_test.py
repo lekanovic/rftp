757e852ee5ee1109a6f7beaecc2bfca0bd806f71
Radovan Lekanovic <lekanovic@gmail.com>
2011-10-13 01:29:00 +0200

    Added new test-case this will test connectivity.
    The test will:
     * connect
     * send large file to server
     * download the large file
     * delete the large file from server
     * close connection
    All this will be done in a infite loop.

---
+++ test/connect_test.py
@@ -0,0 +1,66 @@
+#!/usr/bin/env python
+import ftplib
+import sys
+import time
+
+#http://docs.python.org/library/ftplib.html
+
+ftp = ftplib.FTP()
+
+def print_test_failed():
+	print "\033[01;31mTEST FAILED\033[0m"
+
+def print_test_passed():
+	print "\033[01;34mTEST PASSED\033[0m"
+
+def connect_to_ftp():
+	ftp.connect('localhost',7000)     #specify port number when connection
+	ftp.login('radovan','XXX')
+	ftp.sendcmd('TYPE I')
+
+def stor_cmd_test():
+	filename = 'big.bin'
+	try:
+		f = open(filename,'rb')
+		ftp.storbinary('STOR ' + filename, f)
+		print_test_passed()
+	except Exception,e:
+		print e
+		print_test_failed()
+
+def retr_cmd_test():
+	filename = 'big.bin'
+	try:
+		ftp.retrbinary('RETR ' + filename, open(filename, 'wb').write)
+		print_test_passed()
+	except Exception,e:
+		print e
+		print_test_failed()
+
+def dele_cmd_test():
+	f='big.bin'
+	if ftp.sendcmd('DELE ' + f) != "250 " + f + " has been deleted":
+		print_test_failed()
+	else:
+		print_test_passed() 
+
+def close_connection():
+	ftp.sendcmd('QUIT')
+	ftp.close()
+
+def main(argv):
+	while True:
+		print "*** START NEW TEST ROUND ***"
+		connect_to_ftp()
+		stor_cmd_test()
+		retr_cmd_test()
+		dele_cmd_test()
+		close_connection()
+		time.sleep(2)
+
+if __name__ == "__main__":
+	main(sys.argv[1:])
+
+
+
+
