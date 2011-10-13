731ca243ecc51b818f818b117bc14b2c9b230ff1
Radovan Lekanovic <lekanovic@gmail.com>
2011-10-13 15:33:52 +0200

    Using fork instead of threads when accepting connections.
    Fork is easier to use then threads so we will be using this
    instead.

--- src/setup.c
+++ src/setup.c
@@ -2,38 +2,29 @@
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
-#include <pthread.h>
 #include <string.h>
 #include <stdlib.h>
 #include <errno.h>
 #include <unistd.h>
-
+#include "err_print.h"
 #include "parser.h"
-
-#define THREADS	20
 
 static int sfd;
 
-void* connection(void* argument)
+void connection(void* argument)
 {
 	int client_sfd;
 
 	client_sfd = *((int*)argument);
-	printf("new client, sock =%d\n",client_sfd);
 
 	handle_msg(client_sfd);
-
-	printf("client exit, sock =%d\n",client_sfd);
-	close(client_sfd);
-	pthread_exit(NULL);
 }
 
 int start_server()
 {
-	int client_sfd,i=0,port=7000;
+	int client_sfd,pid=0,port=7000;
 	socklen_t addrlen;
 	struct sockaddr_in server_addr,client_addr;
-	pthread_t threads[THREADS];
 
 	if ((sfd = socket(AF_INET,SOCK_STREAM,0)) < 0)
 		printf("[%s:%s:%d] %s\n",__FILE__,__func__,__LINE__,strerror(errno));
@@ -53,10 +44,22 @@ again:
 	addrlen = sizeof(struct sockaddr_in);
 
 	while(1) {
-		client_sfd = accept(sfd,(struct sockaddr*)&client_addr,&addrlen);
-		pthread_create(&threads[i++], NULL, connection, (void*)&client_sfd);
+		if ((client_sfd = accept(sfd,(struct sockaddr*)&client_addr,&addrlen)) < 0)
+			ERR("accept\n");
+
+		if ((pid = fork()) < 0)
+			ERR("fork\n");
+
+		if (pid == 0) {
+			close(sfd);
+			printf("new client, sock =%d\n",client_sfd);
+			connection(&client_sfd);
+		} else {
+			close(client_sfd);
+			printf("client exit, sock =%d\n",client_sfd);
+		}
 	}
-	pthread_exit(NULL);
+
 	close(sfd);
 }
 
