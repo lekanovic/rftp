99932ffb285f4fe0c1e4590296d7d588a383fc05
Radovan Lekanovic <lekanovic@gmail.com>
2011-10-13 17:54:37 +0200

    Improved ERR message.
    Now the server should call exit if ERR is called. Otherwise
    if an error occures then there's a lot of prints generated
    since then everything will fail.

--- inc/err_print.h
+++ inc/err_print.h
@@ -4,6 +4,10 @@
 #include <stdio.h>
 #include <errno.h>
 #include <string.h>
-#define ERR(fmt, args...) printf("[%s:%s:%d] %s "fmt,__FILE__,__func__,__LINE__,strerror(errno));
+#define ERR(fmt, args...) \
+	do{ \
+		printf("[%s:%s:%d] %s "fmt,__FILE__,__func__,__LINE__,strerror(errno)); \
+		exit(1); \
+	}while(0);
 
 #endif
