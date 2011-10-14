e8447d89879a1262b70b91d0334709f606faf279
Radovan Lekanovic <lekanovic@gmail.com>
2011-10-14 12:40:46 +0200

    Use the real ip and not localhost.
    Server used localhost as ip this has now been changed
    so that it's usig the real ip.

--- src/setup.c
+++ src/setup.c
@@ -1,4 +1,6 @@
 #include <stdio.h>
+#include <netdb.h>
+#include <arpa/inet.h>
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
@@ -22,22 +24,37 @@ void connection(void* argument)
 
 int start_server()
 {
-	int client_sfd,pid=0,port=7000;
+	char hostname[200];
+	int client_sfd,pid=0,port=7000,ret=0;
 	socklen_t addrlen;
 	struct sockaddr_in server_addr,client_addr;
+	struct hostent *he;
 
 	if ((sfd = socket(AF_INET,SOCK_STREAM,0)) < 0)
 		ERR("socket\n");
 again:
+	if ((ret = gethostname(hostname,200)) < 0)
+		ERR("gethostname\n");
+
+	if ((he = gethostbyname(hostname)) == NULL) {
+		ERR("gethostbyname\n");
+	}
+
 	server_addr.sin_family = AF_INET;
-	server_addr.sin_addr.s_addr = INADDR_ANY;
+	memcpy(&server_addr.sin_addr,he->h_addr_list[0],he->h_length);
 	server_addr.sin_port = htons(port);
 
 	if (bind(sfd, (struct sockaddr*)&server_addr,sizeof(server_addr)) < 0) {
 		port++;
 		goto again;
 	}
-	printf("Using port: %d\n",port);
+
+	printf("Server: %s %s %s:%d\n",
+		he->h_name,
+		hostname,
+		inet_ntoa(*((struct in_addr *)he->h_addr)),
+		port);
+
 	if (listen(sfd,3) < 0)
 		ERR("listen\n");
 
@@ -51,9 +68,32 @@ again:
 			ERR("fork\n");
 
 		if (pid == 0) {
-			printf("new client, sock =%d\n",client_sfd);
+			char ipstr[100];
+			struct sockaddr_in disconn_addr;
+			socklen_t addr_len;
+
+			printf("Connected %s:%d\n",
+				inet_ntoa(client_addr.sin_addr),
+				client_sfd);
+
 			connection(&client_sfd);
-			printf("client exit, sock =%d\n",client_sfd);
+
+			/*
+			 * This is an bug in getpeername socklen_t is 8 bytes
+			 * the library code expects 4 bytes so the struct was
+			 * no populated because it was too short. So extra 4
+			 * had to be added.
+			*/
+			addr_len = sizeof(addr_len) + 4;
+
+			if ((ret=getpeername(client_sfd,(struct sockaddr *)&disconn_addr,&addr_len)) < 0 )
+				ERR("getpeername\n");
+
+			inet_ntop(AF_INET,&disconn_addr.sin_addr,ipstr,sizeof(ipstr));
+
+			printf("Disconnected %s:%d\n",
+				ipstr,
+				client_sfd);
 		} else {
 			close(client_sfd);
 		}
