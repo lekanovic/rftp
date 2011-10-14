b3625506e4443f67df529f77a04d6dbf37fb0375
Radovan Lekanovic <lekanovic@gmail.com>
2011-10-14 14:48:30 +0200

    Moved ftp messages to an own header file.
    Ftp message should resist in it's own header file.

--- src/parser.c
+++ src/parser.c
@@ -15,26 +15,7 @@
 #include "file_op.h"
 #include "err_print.h"
 #include "connect.h"
-
-/* CR \r
- * LF \n
- */
-#define LOG_IN_OK		"230 Welcome to server\r\n"
-#define WELCOME_MSG		"220 Raddes ftp server beta v1.0\r\n"
-#define LOGIN_MSG		"331 Anonymous access allowed\r\n"
-#define SYSTEM_TYPE		"215 LINUX-2.6\r\n"
-#define OPEN_ASCII_MODE		"150 Opening ASCII mode data connection\r\n"
-#define TYPE_ASCII_MODE		"200 Opening ASCII mode data connection\r\n"
-#define OPEN_BINARY_MODE	"150 Opening binary mode data connection\r\n"
-#define TYPE_BINARY_MODE	"200 Opening binary mode data connection\r\n"
-#define WORKING_DIR_CHANGED	"200 Working directory changed\r\n"
-#define TRANSFER_COMPLETE	"226 Transfer complete\r\n"
-#define GOODBYE			"221 Goodbye\r\n"
-#define PORT_CMD_OK		"200 PORT command successful\r\n"
-#define START_STOR_CMD		"150 File start to download\r\n"
-#define ENTER_PASSV_MODE	"227 Entering Passive Mode\r\n"
-#define FILE_UNAVAILABLE	"550 Requested action not taken. File unavailable\r\n"
-#define FILE_ACTION_OK		"250 Requested file action okay, completed\r\n"
+#include "ftp_msg.h"
 
 #define BUF_SIZE	1024
 
