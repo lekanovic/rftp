8a6950723238c7d3d0657a43fa65c93ea6f4078c
Radovan Lekanovic <lekanovic@gmail.com>
2011-10-16 13:42:06 +0200

    Extend the ERR message macro to not always exit.
    The ERR macro previous called exit this should not always be done.

--- inc/err_print.h
+++ inc/err_print.h
@@ -4,10 +4,14 @@
 #include <stdio.h>
 #include <errno.h>
 #include <string.h>
+
+#define AGGRESIVE	0
+
 #define ERR(fmt, args...) \
 	do{ \
 		printf("[%s:%s:%d] %s "fmt,__FILE__,__func__,__LINE__,strerror(errno)); \
-		exit(1); \
+		if (AGGRESIVE) \
+			exit(1); \
 	}while(0);
 
 #endif
