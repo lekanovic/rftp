920830ae3b10f5cea1744731284b1524553e835d
Radovan Lekanovic <lekanovic@gmail.com>
2014-04-10 09:43:39 -0700

    Change debug func to support client IP

--- inc/debug_print.h
+++ inc/debug_print.h
@@ -7,10 +7,14 @@
 
 extern int debug_mode;
 
-#define DEBUG_PRINT(format,args...) \
+#define DEBUG_PRINT(sock,fmt, args...) \
 	do{ \
-	      if (debug_mode) \
-			printf(format, ##args); \
+	if (debug_mode) { \
+		char ipstr[20]; \
+		get_ip_address(sock,ipstr); \
+		printf("IP: %s %s:%d "fmt,ipstr,__FILE__,__LINE__,args); \
+	} \
 	}while(0);
 
 #endif
+
