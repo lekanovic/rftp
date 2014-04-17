d85cd07d63f1af6b31a99e10cb3788b474f636cb
Radovan Lekanovic <lekanovic@gmail.com>
2014-04-16 23:05:32 -0700

    Fixing what was removed by misstake before

--- src/parser.c
+++ src/parser.c
@@ -347,11 +347,14 @@ int handle_feat(int cmd_port)
 // DELE <SP> <pathname> <CRLF>
 int handle_dele(int cmd_port,char *msg)
 {
+	int ret;
 	char *send_msg,*file = msg + 5;
 
 	rm_crlf(file);
 
-	dele_dir(file);
+	if ((ret=unlink(file)) < 0) {
+		ERR("unlink");
+	}
 
 	send_msg = ftp_calloc(25 + strlen(file), sizeof(char));
 
