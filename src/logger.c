c4117294a5c5058e6cf29a65835dfea6bf882379
Radovan Lekanovic <radovan.lekanovic@room5.com>
2014-12-01 10:38:39 -0800

    Use malloc instead of static array.

--- src/logger.c
+++ src/logger.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <time.h>
+#include "mem_op.h"
 #include "logger.h"
 #include "helpers.h"
 
@@ -11,26 +12,35 @@ void initlogfile(struct configs* cfg)
 }
 void filelog(int client_fd, char* cmd)
 {
-#define BUF_SIZE 100
-    char buffer[BUF_SIZE];
-    char ipstr[20];
+#define IP_ADDR_LEN 16
+    char *buffer = NULL;
+    char ipstr[IP_ADDR_LEN];
     char* tmp;
+    int size=0;
     time_t ltime;
 
     ltime = time(NULL);
-    memset(buffer,0,BUF_SIZE);
-    memset(ipstr,0,20);
+
+    memset(ipstr,0,IP_ADDR_LEN);
 
     get_ip_address(client_fd, ipstr);
     
     tmp = asctime(localtime(&ltime));
     tmp[strlen(tmp) - 1] = 0;
 
+    size = strlen(cmd)+1+strlen(tmp)+IP_ADDR_LEN;
+
     rm_crlf(cmd);
+
+    buffer = ftp_alloc(size) ;
+
+    memset(buffer,0,size);
 
     sprintf(buffer,"|%s|%s|%s|\n",tmp,ipstr,cmd);
 
     fputs(buffer,logfile);
+
+    ftp_free(buffer);
 }
 void closelogfile()
 {
